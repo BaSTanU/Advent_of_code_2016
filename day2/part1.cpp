0bb9820a9086d7e23eea7d179ef1af50ff858328
Mocanu George <mocanugeorge27@yahoo.com>
2017-02-13 22:11:43 +0200

    Added day2

--- day2/part1.cpp
+++ day2/part1.cpp
@@ -2,35 +2,35 @@
 
 using namespace std;
 
-char sir[100000];
-char cod[100];
-int n;
-
 int main()
 {
-    freopen("data.in", "r", stdin);
+    ifstream fin("data.in");
 
-    int dx = 0, dy = 0;
+    string str;
 
-    while (gets(sir)) {
-        for (int i = 0 ; sir[i]; ++ i) {
-            if (sir[i] == 'U')
-                dx = max(-1, dx - 1);
+    int x = 0, y = 0;
 
-            if (sir[i] == 'R')
-                dy = min(1, dy + 1);
+    cout << "The answer for part1 is: ";
 
-            if (sir[i] == 'D')
-                dx = min(1, dx + 1);
+    while (fin >> str) {
+        for (int i = 0 ; i < str.size(); ++ i) {
+            if (str[i] == 'U')
+                y = max(-1, y - 1);
 
-            if (sir[i] == 'L')
-                dy = max(-1, dy - 1);
+            if (str[i] == 'R')
+                x = min(1, x + 1);
+
+            if (str[i] == 'D')
+                y = min(1, y + 1);
+
+            if (str[i] == 'L')
+                x = max(-1, x - 1);
         }
-        cod[n ++] = (5 + dx * 3 + dy) + '0';
+
+        cout << 5 + y * 3 + x;
     }
 
-    cod[n] = '\0';
-    puts(cod);
+    cout << "\n";
 
     return 0;
 }
--- day2/part2.cpp
+++ day2/part2.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-char sir[100000];
-char cod[100];
 char mat[7][7] = {'0', '0', '0', '0', '0', '0', '0',
                   '0', '0', '0', '1', '0', '0', '0',
                   '0', '0', '2', '3', '4', '0', '0',
@@ -11,33 +9,36 @@ char mat[7][7] = {'0', '0', '0', '0', '0', '0', '0',
                   '0', '0', 'A', 'B', 'C', '0', '0',
                   '0', '0', '0', 'D', '0', '0', '0',
                   '0', '0', '0', '0', '0', '0', '0'};
-int n;
 
 int main()
 {
-    freopen("data.in", "r", stdin);
+    ifstream fin("data.in");
+
+    string str;
 
     int x = 3, y = 1;
 
-    while (gets(sir)) {
-        for (int i = 0 ; sir[i]; ++ i) {
-            if (sir[i] == 'U' && mat[x - 1][y] != '0')
+    cout << "The answer for part2 is: ";
+
+    while (fin >> str) {
+        for (int i = 0 ; i < str.size(); ++ i) {
+            if (str[i] == 'U' && mat[x - 1][y] != '0')
                 -- x;
 
-            if (sir[i] == 'R' && mat[x][y + 1] != '0')
+            if (str[i] == 'R' && mat[x][y + 1] != '0')
                 ++ y;
 
-            if (sir[i] == 'D' && mat[x + 1][y] != '0')
+            if (str[i] == 'D' && mat[x + 1][y] != '0')
                 ++ x;
 
-            if (sir[i] == 'L' && mat[x][y - 1] != '0')
+            if (str[i] == 'L' && mat[x][y - 1] != '0')
                 -- y;
         }
-        cod[n ++] = mat[x][y];
+        
+        cout << mat[x][y];
     }
 
-    cod[n] = '\0';
-    puts(cod);
+    cout << "\n";
 
     return 0;
 }
